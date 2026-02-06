35a330169165ce37afd54e2b1eaa9aa77715bc0a
Bruno <aluno24706@ipt.pt>
2026-02-06 05:39:36 +0000

    Função de adição de compromissos

--- app/src/main/java/pt/ipt/dailysync/MainActivity.kt
+++ app/src/main/java/pt/ipt/dailysync/MainActivity.kt
@@ -1,26 +1,57 @@
 package pt.ipt.dailysync
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
+    private val compromissos = mutableListOf<Compromisso>()
+    private lateinit var adapter: CompromissoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val recycler = findViewById<RecyclerView>(R.id.recyclerCompromissos)
+        val btnAdicionar = findViewById<Button>(R.id.btnAdicionar)
 
-        val compromissos = listOf(
-            Compromisso("Aula de DAM", "05/02/2026"),
-            Compromisso("Trabalho de grupo", "07/02/2026"),
-            Compromisso("Estudar para teste", "10/02/2026")
+        compromissos.add(
+            Compromisso("Aula de DAM", "05/02/2026")
         )
 
+        adapter = CompromissoAdapter(compromissos)
         recycler.layoutManager = LinearLayoutManager(this)
-        recycler.adapter = CompromissoAdapter(compromissos)
+        recycler.adapter = adapter
+
+        btnAdicionar.setOnClickListener {
+            mostrarDialogAdicionar()
+        }
+    }
+
+    private fun mostrarDialogAdicionar() {
+        val layout = layoutInflater.inflate(R.layout.dialog_adicionar, null)
+
+        val etTitulo = layout.findViewById<EditText>(R.id.etTitulo)
+        val etData = layout.findViewById<EditText>(R.id.etData)
+
+        AlertDialog.Builder(this)
+            .setTitle("Novo compromisso")
+            .setView(layout)
+            .setPositiveButton("Adicionar") { _, _ ->
+                val titulo = etTitulo.text.toString()
+                val data = etData.text.toString()
+
+                if (titulo.isNotEmpty() && data.isNotEmpty()) {
+                    compromissos.add(Compromisso(titulo, data))
+                    adapter.notifyItemInserted(compromissos.size - 1)
+                }
+            }
+            .setNegativeButton("Cancelar", null)
+            .show()
     }
 }
-
