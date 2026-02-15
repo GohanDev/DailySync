d52215e6be8bb743cffe644adca886b0a29a5787
Bruno <aluno24706@ipt.pt>
2026-02-15 16:50:56 +0000

    #Extra Update Atualização README.md e página "sobre" Implementação do Render em vez do ngrok

--- app/src/main/java/pt/ipt/dailysync/AboutActivity.kt
+++ app/src/main/java/pt/ipt/dailysync/AboutActivity.kt
@@ -1,22 +1,35 @@
 package pt.ipt.dailysync
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import pt.ipt.dailysync.databinding.ActivityAboutBinding
 
 class AboutActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityAboutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // Botão voltar
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "Sobre"
-    }
+        binding.textCurso.text = "Curso: Engenharia Informática"
+        binding.textDisciplina.text = "Disciplina: Desenvolvimento de Aplicações Móveis"
+        binding.textAno.text = "Ano Letivo: 2025/2026"
 
-    override fun onSupportNavigateUp(): Boolean {
-        finish()
-        return true
+        binding.textAutor.text = """
+            Nº: 24706
+            Nome: Bruno Alves
+        """.trimIndent()
+
+        binding.textTecnologias.text = """
+            Tecnologias utilizadas:
+            - Kotlin
+            - Android Studio
+            - Retrofit
+            - Node.js
+            - Express
+            - Render (deploy API)
+        """.trimIndent()
     }
 }
